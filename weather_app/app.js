a79063585db66f866dfbd720a4622b9ddc4def9b
Cristi Neames <crisneames.gmail.com>
2020-02-11 18:35:00 -0600

    fixed modal and added message to prevent an empty input field

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,37 +1,40 @@
-//console.log('It works!')
+
 
 // hide and show openModal
 
 // Grabbing About button
 const $openBtn = $('#openModal');
-
 // Grabbing modal element
 const $modal = $('#modal');
-
 // Grabbing close button
 const $closeBtn = $('#close');
 
 // Event handler to open the modal
-const openModal = () => {
+const openModal = (event) => {
+  //event.preventDefault();
   $modal.css('display', 'block');
 }
-
 // Event handler to close the modal
 const closeModal = () => {
   $modal.css('display', 'none');
 }
-
 //Add event listener to About button
 $openBtn.on('click', openModal);
-
 //Add event listener to Close button
 $closeBtn.on('click', closeModal);
 
+// Timeout so modal will show after 5 Seconds
 setTimeout(openModal, 5000);
 
+// on click event
 $('#enterZip').on("click", (event)=>{
   event.preventDefault();
+
   let zipCode = $('#zip').val();
+
+  if (zipCode === '') {
+    alert('Not a valid zip code')
+  }
 
     $.when(
           // current weather
@@ -44,11 +47,11 @@ $('#enterZip').on("click", (event)=>{
                  $("#zip").val('');
               }
           }),
-          // 5 day forcast
+          // 3 hour forecast
           $.ajax({
               url: `http://api.openweathermap.org/data/2.5/forecast?zip=${zipCode}&appid=552f303f659425cbc193137deb0f8bbc&units=imperial`,
               success: (data) => {
-                //  alert('5 day forcast complete')
+                //  alert('3 hour forcast complete')
                   getForcastedWeather(data)
                   $("#zip").val('');
               }
@@ -57,30 +60,31 @@ $('#enterZip').on("click", (event)=>{
           //alert('all complete');
   });
 });
+// get current weather
 const getCurrentWeather = (data) => {
-  //console.log(`Weather: ${data.weather[0].description}`)
-
   // Dispay weather description
   $('#description').html(data.weather[0].description);
   // Display temperature
   $('#temperature').html(Math.floor(data.main.temp)) + "°<span>F</span>";
-
-
   // Display city
   $('#city').html(data.name);
-
   // Display icon
   icon = data.weather[0].icon
-  let iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
-  $("#icon").attr("src", iconurl);
-}
+  // let iconurl = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+// let iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
 
+  $("#icon").attr("src", `http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+}
+// get 3 hour forecast
 const getForcastedWeather = (data) => {
+$('.bottom').empty();
+
 
     for (let i = 0; i < 5; i++) {
     // Put date in a variable
+
     const date = (data.list[i].dt)
-    // table for 3 hour forcasted weather
+    // create table for 3 hour forcasted weather
     const $list = $('<table>').addClass("list-container")
           $('.bottom').append($list)
     // row
@@ -95,47 +99,41 @@ const getForcastedWeather = (data) => {
     // row
     const $row2 = $('<tr>')
           $('.list-container').append($row2)
-    // image
+    // icons
     const iconurl = (`<img src=http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png />`);
     const $icon = $(`<td> id="wicon"`)
           $("#icon").attr("src", iconurl);
           $($icon).html(iconurl)
           $row2.append($icon)
+    // temperature
     const $temp = $(`<td id="${data[i]}-temperature">${Math.floor(data.list[i].main.temp)}</td>`);
+    //description
     $row2.append($temp)
     const $desc = $(`<td id="${data[i]}-desc">${data.list[i].weather[0].description}</td>`);
      $row2.append($desc)
  }
 }
-
+// converts hex into date
 const dateConverter = (dateInHex) => {
 
   // array of months
   const months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-
   // Convert timestamp to milliseconds
  let date = new Date(dateInHex*1000);
-
  // Year
  let year = date.getFullYear();
-
  // Month
  let month = months_arr[date.getMonth()];
-
  // Day
  let day = date.getDate();
-
  // Hours
  let hours = date.getHours();
-
  // Minutes
  let minutes = "0" + date.getMinutes();
-
  // Seconds
  let seconds = "0" + date.getSeconds();
-
  // Display date time in MM-dd-yyyy h:m:s format
  let convdataTime = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
+ // returns date with hours and minutes
  return convdataTime
 }
